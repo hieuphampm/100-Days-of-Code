f6d1502ebe13501ef297a1703668cec0bdb55513
hieuphampm <hieuphamminh976@gmail.com>
2023-11-20 20:28:43 +0700

    init project

---
+++ Day 1.py
@@ -0,0 +1,41 @@
+# printing start
+#print("Hello world!")
+
+# \n
+#print("Hello world\nHello world")
+
+# combine
+#print("hello" + " " + "Axel")
+
+# input
+# input("What is your name? ")
+# print("Hello " + input("What is your name? "))
+
+# count
+#print(len(input()))
+
+# variable
+# name = input("What is your name?")
+# print(name)
+
+# name = input("Your name?")
+# length = len(name)
+# print(length)
+
+# variables to manage data
+# a = input()
+# b = input()
+
+# c = a
+# a = b
+# b = c
+
+# print("a " + a)
+# print("b " + b)
+
+# band name generator
+print("Welcome to Band name generator")
+city = input("Where is your city?\n")
+name = input("what is your pet name?\n")
+combine = "Your Band name could be " + name + " " + city
+print(combine)
